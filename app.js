61225d22fb53346f56818ded18b08fef440e07cb
minnji88 <kkhhjj3393@gmail.com>
2021-01-30 01:06:30 +0900

    #4. nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-// express 모듈을 가져온다.(require)
 const express = require('express');
+// nunjucks 모듈은 app.js에서
+const nunjucks = require('nunjucks');
+
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
 
-// 모듈은 함수라서 애플리케이션이라는 것을 리턴한다. 
-// 그 리턴한 값을 제어하기 위해서 app 객체를 생성한다.
 const app = express();
 const port = 3000;
 
-// 사용자가 홈페이지에 접속할 때는 GET POST
-// GET(URL로 접속하는 방식) : 요청 화면에 이렇게 보여줘라!
+nunjucks.configure('template', {
+    autoescape : true,
+    express: app
+});
+// template를 인식하고 사용하겠다
+// autoescape는 보안상 true
+// express : app는 사용할 주체
+
 app.get('/express', (req, res) => {
     res.send('express start');
 });
