0453668809d14822744200e02df6f562e8584fd7
minnji88 <kkhhjj3393@gmail.com>
2021-01-31 23:51:03 +0900

    #10. 404 & 500 handling

--- app.js
+++ app.js
@@ -18,13 +18,11 @@ nunjucks.configure('template', {
 app.use( logger('dev') );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended : false }) );
-// uploads 파일을 정적파일로!
-// 앞이 url + 뒤 폴더이름
 app.use('/uploads' , express.static('uploads'));
 
-// global View Variable 
+// global 변수 세팅
 app.use((req, res, next)=>{
-    app.locals.isLogin = true;
+    app.locals.isLogin = true; // 로그인 여부
     next();
 });
 
@@ -35,6 +33,14 @@ app.get('/', (req, res) => {
 // Routing
 app.use('/admin',  admin);
 app.use('/contacts', contacts);
+
+app.use((req, res, _ ) => {
+    res.status(400).render('common/404.html');
+});
+
+app.use((req, res, _ ) => {
+    res.status(500).render('common/500.html');
+});
 
 app.listen(port, ()=> {
     console.log('Express listening on port', port)
